21d221f9d6d37d2e3d552151299d5364f867ad70
Charliego93 <charlie.go.993@gmail.com>
2023-03-16 23:28:12 +0800

    Update: add keymapping

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -74,6 +74,7 @@ M.setup = function ()
   vim.opt.shortmess:append "c" -- don't show redundant messages from ins-completion-menu
   vim.opt.shortmess:append "I" -- don't show the default intro message
   vim.opt.whichwrap:append "<,>,[,],h,l"
+  require("core.keymappings")
 
   for k, v in pairs(default_opts) do
     vim.opt[k] = v
--- lua/core/keymappings.lua
+++ lua/core/keymappings.lua
@@ -1,85 +1,61 @@
-local generic_opts_any = { noremap = true, silent = true }
-local defaults = {
-    i = {
-        keys = {
-            -- Move current line / block with Alt-j/k ala vscode.
-            ["<A-j>"] = "<Esc>:m .+1<CR>==gi",
-            -- Move current line / block with Alt-j/k ala vscode.
-            ["<A-k>"] = "<Esc>:m .-2<CR>==gi",
-            -- navigation
-            ["<A-Up>"] = "<C-\\><C-N><C-w>k",
-            ["<A-Down>"] = "<C-\\><C-N><C-w>j",
-            ["<A-Left>"] = "<C-\\><C-N><C-w>h",
-            ["<A-Right>"] = "<C-\\><C-N><C-w>l",
-        },
-        opts = generic_opts_any,
-    },
-    n = {
-        keys = {
-            -- Better window movement
-            ["<C-h>"] = "<C-w>h",
-            ["<C-j>"] = "<C-w>j",
-            ["<C-k>"] = "<C-w>k",
-            ["<C-l>"] = "<C-w>l",
-            -- Resize with arrows
-            ["<C-Up>"] = ":resize -2<CR>",
-            ["<C-Down>"] = ":resize +2<CR>",
-            ["<C-Left>"] = ":vertical resize -2<CR>",
-            ["<C-Right>"] = ":vertical resize +2<CR>",
-            -- Move current line / block with Alt-j/k a la vscode.
-            ["<A-j>"] = ":m .+1<CR>==",
-            ["<A-k>"] = ":m .-2<CR>==",
-            -- QuickFix
-            ["]q"] = ":cnext<CR>",
-            ["[q"] = ":cprev<CR>",
-            ["<C-q>"] = ":call QuickFixToggle()<CR>",
-        },
-        opts = generic_opts_any,
-    },
-    t = {
-        keys = {
-            -- Terminal window navigation
-            ["<C-h>"] = "<C-\\><C-N><C-w>h",
-            ["<C-j>"] = "<C-\\><C-N><C-w>j",
-            ["<C-k>"] = "<C-\\><C-N><C-w>k",
-            ["<C-l>"] = "<C-\\><C-N><C-w>l",
-        },
-        opts = { silent = true },
-    },
-    v = {
-        keys = {
-            -- Better indenting
-            ["<"] = "<gv",
-            [">"] = ">gv",
-            -- ["p"] = '"0p',
-            -- ["P"] = '"0P',
-        },
-        opts = generic_opts_any,
-    },
-    x = {
-        keys = {
-            -- Move current line / block with Alt-j/k ala vscode.
-            ["<A-j>"] = ":m '>+1<CR>gv-gv",
-            ["<A-k>"] = ":m '<-2<CR>gv-gv",
-        },
-        opts = generic_opts_any,
-    },
-    c = {
-        keys = {
-            -- navigate tab completion with <c-j> and <c-k>
-            -- runs conditionally
-            ["<C-j>"] = { 'pumvisible() ? "\\<C-n>" : "\\<C-j>"', { expr = true, noremap = true } },
-            ["<C-k>"] = { 'pumvisible() ? "\\<C-p>" : "\\<C-k>"', { expr = true, noremap = true } },
-        },
-        opts = generic_opts_any,
-    },
-}
+local opts = { noremap = true, silent = true }
 
-for mode, mappings in pairs(defaults) do
-    vim.notify(mode .. mappings)
-    local opts = mappings.opts
-    for key, val in pairs(mappings.keys) do
-        pcall(vim.api.nvim_del_keymap, mode, key)
-        vim.keymap.set(mode, key, val, opts)
-    end
-end
+-- Move current line / block with Alt-j/k ala vscode.
+vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+-- Move current line / block with Alt-j/k ala vscode.
+vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+-- navigation
+vim.keymap.set("i", "<A-Up>", "<C-\\><C-N><C-w>k", opts)
+vim.keymap.set("i", "<A-Down>", "<C-\\><C-N><C-w>j", opts)
+vim.keymap.set("i", "<A-Left>", "<C-\\><C-N><C-w>h", opts)
+vim.keymap.set("i", "<A-Right>", "<C-\\><C-N><C-w>l", opts)
+
+-- Better window movement
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
+-- Resize with arrows
+vim.keymap.set("n", "<A-Up>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<A-Down>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<A-Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<A-Right>", ":vertical resize +2<CR>", opts)
+-- copy & paste
+vim.keymap.set("n", "<C-c>", "vy", opts)
+vim.keymap.set("n", "<C-v>", "p", opts)
+-- Move current line / block with Alt-j/k a la vscode.
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", opts)
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", opts)
+vim.keymap.set("n", "]q", ":cnext<CR>", opts)
+vim.keymap.set("n", "[q", ":cprev<CR>", opts)
+vim.keymap.set("n", "<C-q>", ":call QuickFixToggle()<CR>", opts)
+vim.keymap.set("n", "<C-s>", ":w<CR>", opts)
+-- navigation on nomal mode
+vim.keymap.set("n", "<", "v<gv", opts)
+vim.keymap.set("n", ">", "v>gv", opts)
+
+-- Terminal window navigation
+vim.keymap.set("t", "<C-h>", "<C-\\><C-N><C-w>h", opts)
+vim.keymap.set("t", "<C-j>", "<C-\\><C-N><C-w>j", opts)
+vim.keymap.set("t", "<C-k>", "<C-\\><C-N><C-w>k", opts)
+vim.keymap.set("t", "<C-l>", "<C-\\><C-N><C-w>l", opts)
+
+-- Better indenting
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", "<Tab>", ">gv", opts)
+vim.keymap.set("v", "<S-Tab>", "<gv", opts)
+-- vim.keymap.set('v', 'p', '"0p', opts)
+-- vim.keymap.set('v', 'P', '"0P', opts)
+-- visual mode copy & paste
+vim.keymap.set("v", "<C-c>", "y", opts)
+vim.keymap.set("v", "<C-v>", "p", opts)
+
+-- Move current line / block with Alt-j/k ala vscode.
+vim.keymap.set("x", "<A-j>", ":m '>+1<CR>gv-gv", opts)
+vim.keymap.set("x", "<A-k>", ":m '<-2<CR>gv-gv", opts)
+
+-- navigate tab completion with <c-j> and <c-k>
+opts = vim.tbl_extend("force", opts, { expr = true, noremap = true })
+vim.keymap.set("c", "<C-j>", 'pumvisible() ? "\\<C-n>" : "\\<C-j>"', opts)
+vim.keymap.set("c", "<C-k>", 'pumvisible() ? "\\<C-p>" : "\\<C-k>"', opts)
