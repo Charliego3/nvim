17d1db5f2038efee4ec088b97a16c41761a9405e
Charliego93 <charlie.go.993@gmail.com>
2023-03-22 18:43:16 +0800

    Update: NoName buf closed

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
-require('core.neovide')
+require("core.neovide")
 
-if vim.fn.has "nvim-0.8" ~= 1 then
+if vim.fn.has("nvim-0.8") ~= 1 then
     vim.notify("Please upgrade your Neovim base installation. Requires v0.8+", vim.log.levels.WARN)
     vim.wait(1000, function()
     end)
-    vim.cmd "cquit"
+    vim.cmd("cquit")
 end
 
 local base_dir = vim.env.NVIM_BASE_DIR
@@ -29,3 +29,12 @@ end
 
 require("core").setup()
 require("core.plugins")
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function()
+        local bufname = vim.api.nvim_buf_get_name(0)
+        if bufname == "NoName" then
+            vim.cmd("close")
+        end
+    end,
+})
